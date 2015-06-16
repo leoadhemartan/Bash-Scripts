771bf95fa4332a835d0aaa89c918957188d58229
Leo Adhemar Tan <leoadhemartan@users.noreply.github.com>
2015-06-16 13:37:37 +0800

    Update rmimg.sh
    added comments

--- rmimg.sh
+++ rmimg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for i in file1 file2 file3
+for i in file1 file2 file3 #names are intended to be the first part of the file name
 do
    echo "Finding files starting in $i"
-   find . -name "$i*"
+   find . -name "$i*"  #because I just want a message that the image file was found
    echo "Deleting files starting in $i"
    find . -name "$i*" -exec rm {} \; 
 done
